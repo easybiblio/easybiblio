24cb22c9b5fcfcf2bf92207cd5ef532ed3d32fc9
fmfurtado <fabiofurtado10-github@yahoo.fr>
2014-12-18 17:04:01 +0100

    Translation: person.php translated

--- server/person.php
+++ server/person.php
@@ -1,6 +1,6 @@
 <?php include '_header.php' ?>
 
-<h1>Personne</h1>
+<h1><?= $t->__('db.person') ?></h1>
 
 <?php
   $id = $_GET['id'];
@@ -17,43 +17,43 @@
 <table width="70%" border="0">
 
   <tr>
-    <td width="10%"><strong>Nom/Prénom:</strong></td>
+    <td width="10%"><strong><?= $t->__('db.person.name') ?>:</strong></td>
     <td><input type="text" name="name" value=<?= $fmw->getPostOrArrayQuoted($columns, 'name') ?> size="52" maxlength="120"></td>
   </tr>
   
   <tr>
-    <td><strong>Address:</strong></td>
+    <td><strong><?= $t->__('db.person.address') ?>:</strong></td>
     <td><input type="text" name="address" value=<?= $fmw->getPostOrArrayQuoted($columns, 'address') ?> size="52" maxlength="120"></td>
   </tr>
 
   <tr>
-    <td>Code Postal:</td>
+    <td><?= $t->__('db.person.zipcode') ?>:</td>
     <td> 
       <input type="text" name="zipcode" value=<?= $fmw->getPostOrArrayQuoted($columns, 'zipcode') ?> size="52" maxlength="10"></td>
   </tr>
   
   <tr>
-    <td>Ville:</td>
+    <td><?= $t->__('db.person.city') ?>:</td>
     <td><input type="text" name="city" value=<?= $fmw->getPostOrArrayQuoted($columns, 'city') ?> size="52" maxlength="45"></td>
   </tr>
 
   <tr>
-    <td>Phone 1:</td>
+    <td><?= $t->__('db.person.phone1') ?>:</td>
     <td><input type="text" name="phone1" value=<?= $fmw->getPostOrArrayQuoted($columns, 'phone1') ?> size="52" maxlength="45"></td>
   </tr>
   
   <tr>
-    <td>Phone 2:</td>
+    <td><?= $t->__('db.person.phone2') ?>:</td>
     <td><input type="text" name="phone2" value=<?= $fmw->getPostOrArrayQuoted($columns, 'phone2') ?> size="52" maxlength="45"></td>
   </tr>
     
   <tr>
-    <td>E-mail:</td>
+    <td><?= $t->__('db.person.email') ?>:</td>
     <td><input type="text" name="email" value=<?= $fmw->getPostOrArrayQuoted($columns, 'email') ?> size="52" maxlength="100"></td>
   </tr>
     
   <tr>
-    <td>Notes:</td>
+    <td><?= $t->__('db.person.notes') ?>:</td>
     <td>
         <textarea rows="6" cols="47" name="notes"><?= $fmw->escapeHtml($fmw->getPostOrArray($columns, 'notes')) ?></textarea>
     </td>
@@ -61,7 +61,7 @@
     
   <?php if ($id != '') { ?>
   <tr>
-    <td>Date&nbsp;Creation:</td>
+    <td><?= $t->__('db.person.date_creation') ?>:</td>
     <td><?= $fmw->getPostOrArray($columns, 'date_creation') ?></td>
   </tr>
   <?php } ?>
@@ -76,8 +76,8 @@
   <tr> 
     <td>&nbsp;</td>
     <td align="left" class="texte"> 
-      <input type="submit" name="Submit" value="Sauvegarder">
-      <input type="button" value="Annuler" onclick="window.location.href='personSearch.php'"></td>
+      <input type="submit" name="Submit" value="<?= $t->__('button.save') ?>">
+      <input type="button" value="<?= $t->__('button.cancel') ?>" onclick="window.location.href='personSearch.php'"></td>
   </tr>
 
 </table>
