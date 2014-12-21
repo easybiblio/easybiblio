d95a168a927575f8be8d93deb6886bf439787888
fmfurtado <fabiofurtado10-github@yahoo.fr>
2014-12-21 16:12:29 +0100

    Translator function __() now accepts two extra arguments

--- server/_translator.php
+++ server/_translator.php
@@ -18,8 +18,8 @@ class Translator {
 	private function splitStrings($str) {
         return explode('=', trim($str));
     }
-	
-	public function __($str) {	
+    
+	public function __($str, $arg1 = "", $arg2 = "") {
         if (!array_key_exists($this->language, $this->lang)) {
             $filename = 'lang/'.$this->language.'.txt';
             if (file_exists($filename)) {
@@ -27,13 +27,26 @@ class Translator {
                 foreach ($strings as $k => $v) {
 					$this->lang[$this->language][$v[0]] = $v[1];
                 }
-                return $this->findString($str);
+                $toReturn = $this->findString($str);
             } else {
-                return $str;
+                $toReturn = $str;
             }
         } else {
-            return $this->findString($str);
+            $toReturn = $this->findString($str);
         }
+        
+        // Documentation to generate: <body text='black'>
+        // $bodytag = str_replace("%1", "black", "<body text='%1'>");
+        
+        if (strpos($toReturn, "%1") !== false && $arg1 != "") {
+            $toReturn = str_replace("%1", $arg1, $toReturn);
+        }
+        
+        if (strpos($toReturn, "%2") !== false && $arg2 != "") {
+            $toReturn = str_replace("%2", $arg2, $toReturn);
+        }
+        
+        return $toReturn;
     }
 }
 ?>
