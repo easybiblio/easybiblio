36e180f079e8d9f9d4adbc88f1c56f2e1abfb78c
fmfurtado <fabiofurtado10-github@yahoo.fr>
2015-03-04 21:45:49 +0100

    Using bootstrap in book.php

--- server/book.php
+++ server/book.php
@@ -71,135 +71,147 @@ function loadImage() {
 
 ?>
 
-<form action="bookSave.php" method="post">
+<form action="bookSave.php" class="form-horizontal" method="post">
 
 <!-- ID -->
 <input type="hidden" name="id" value="<?= $id ?>">
 
-<table>
-<tr><td>
-<table width="70%" border="0">
+<div class="row">
+<div class="col-sm-7">
 
-  <!-- Code -->
-  <tr>
-    <td width="10%"><strong><?= $t->__('db.book.code') ?>:</strong></td>
-    <td><input type="text" name="code" value=<?= $fmw->getPostOrArrayQuoted($columns, 'code') ?> size="52" maxlength="45"></td>
-  </tr>
+  <!-- Code --> 
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.code') ?>:</label>
+    <div class="col-sm-10">
+        <input type="text" class="form-control" name="code" value=<?= $fmw->getPostOrArrayQuoted($columns, 'code') ?>  maxlength="45">
+    </div>
+  </div>
 
   <!-- Type -->
-  <tr>
-    <td><?= $t->__('db.book.type') ?>:</td>
-    <td>
-    <select name="type_id" size="1">
-        <?php prepareTypeOptions( $fmw->getPostOrArray($columns, 'type_id') ); ?>
-	</select>
-    </td>
-  </tr>
-    
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.type') ?>:</label>
+    <div class="col-sm-10">
+        <select name="type_id" class="form-control" size="1">
+            <?php prepareTypeOptions( $fmw->getPostOrArray($columns, 'type_id') ); ?>
+        </select>
+    </div>
+  </div>
+
   <!-- Title -->
-  <tr>
-    <td><strong><?= $t->__('db.book.title') ?>:</strong></td>
-    <td> 
-      <input type="text" name="title" value=<?= $fmw->getPostOrArrayQuoted($columns, 'title') ?> size="52" maxlength="100"></td>
-  </tr>
-  
-  <tr>
-    <td><?= $t->__('db.book.description') ?>:</td>
-    <td>
-        <textarea rows="6" cols="47" name="description"><?= $fmw->escapeHtml($fmw->getPostOrArray($columns, 'description')) ?></textarea>
-    </td>
-  </tr>
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.title') ?>:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="title" value=<?= $fmw->getPostOrArrayQuoted($columns, 'title') ?>  maxlength="100">
+    </div>
+  </div>
+    
+  <!-- Description -->
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.description') ?>:</label>
+    <div class="col-sm-10">
+        <textarea class="form-control" rows="5" name="description"><?= $fmw->escapeHtml($fmw->getPostOrArray($columns, 'description')) ?></textarea>
+    </div>
+  </div>
     
   <!-- Author -->
-  <tr>
-    <td><?= $t->__('db.book.author') ?>:</td>
-    <td> 
-      <input type="text" name="author" value=<?= $fmw->getPostOrArrayQuoted($columns, 'author') ?> size="52" maxlength="100"></td>
-  </tr>
-  
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.author') ?>:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="author" value=<?= $fmw->getPostOrArrayQuoted($columns, 'author') ?> maxlength="100">
+    </div>
+  </div>
+    
   <!-- CoAuthor -->
-  <tr>
-    <td><?= $t->__('db.book.coauthor') ?>:</td>
-    <td><input type="text" name="coauthor" value=<?= $fmw->getPostOrArrayQuoted($columns, 'coauthor') ?> size="52" maxlength="100"></td>
-  </tr>
-  
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.coauthor') ?>:</label>
+    <div class="col-sm-10">
+        <input type="text" class="form-control" name="coauthor" value=<?= $fmw->getPostOrArrayQuoted($columns, 'coauthor') ?> maxlength="100">
+    </div>
+  </div>
+    
   <!-- Editor -->
-  <tr>
-    <td><?= $t->__('db.book.editor') ?>:</td>
-    <td><input type="text" name="editor" value=<?= $fmw->getPostOrArrayQuoted($columns, 'editor') ?> size="52" maxlength="100"></td>
-  </tr>
- 
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.editor') ?>:</label>
+    <div class="col-sm-10">
+<input type="text" class="form-control" name="editor" value=<?= $fmw->getPostOrArrayQuoted($columns, 'editor') ?> maxlength="100">
+    </div>
+  </div>
+    
   <!-- Year of publication -->
-  <tr>
-    <td><?= $t->__('db.book.year_publication') ?>:</td>
-    <td><input type="number" name="year_publication" value=<?= $fmw->getPostOrArrayQuoted($columns, 'year_publication') ?> size="52" maxlength="4"></td>
-  </tr>
-  
-
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.year_publication') ?>:</label>
+    <div class="col-sm-10">
+<input type="number" class="form-control" name="year_publication" value=<?= $fmw->getPostOrArrayQuoted($columns, 'year_publication') ?>  maxlength="4">
+    </div>
+  </div>
+    
   <!-- Language -->
-  <tr>
-    <td><?= $t->__('db.book.language') ?>:</td>
-    <td>
-        <select name="language" size="1">
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.language') ?>:</label>
+    <div class="col-sm-10">
+        <select class="form-control" name="language">
             <?php prepareLanguageOptions( $fmw->getPostOrArray($columns, 'language') ); ?>
 	    </select>
-    </td>
-  </tr>
+    </div>
+  </div>
     
   <!-- Category -->
-  <tr>
-    <td><?= $t->__('db.book.category') ?>:</td>
-    <td>
-    <select name="category_id" size="1">
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.category') ?>:</label>
+    <div class="col-sm-10">
+    <select class="form-control" name="category_id">
         <?php prepareCategoryOptions( $fmw->getPostOrArray($columns, 'category_id') ); ?>
 	</select>
-    </td>
-  </tr>
+    </div>
+  </div>
+        
+  <!-- Notes -->
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.notes') ?>:</label>
+    <div class="col-sm-10">
+        <textarea class="form-control" rows="5" cols="47" name="notes"><?= $fmw->escapeHtml($fmw->getPostOrArray($columns, 'notes')) ?></textarea>
+    </div>
+  </div>
     
-  <tr>
-    <td><?= $t->__('db.book.notes') ?>:</td>
-    <td>
-        <textarea rows="6" cols="47" name="notes"><?= $fmw->escapeHtml($fmw->getPostOrArray($columns, 'notes')) ?></textarea>
-    </td>
-  </tr>
-    
-   <?php if ($id != '') { ?>
-  <tr>
-    <td><?= $t->__('db.book.date_creation') ?>:</td>
-    <td><?= $fmw->getPostOrArray($columns, 'date_creation') ?></td>
-  </tr>
+  <?php if ($id != '') { ?>
+  <!-- Creation date -->
+  <div class="form-group">
+    <label class="control-label col-md-2"><?= $t->__('db.book.date_creation') ?>:</label>
+    <div class="col-sm-10">
+        <p class="form-control-static"><?= $fmw->getPostOrArray($columns, 'date_creation') ?></p>
+    </div>
+  </div>
   <?php } ?>
 
-  <!-- Ligne vide -->
-  <tr>
-    <td valign="top">&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-
-  <!-- Buttons -->
-  <tr> 
-    <td>&nbsp;</td>
-    <td align="left" class="texte">
-      <?php if ($fmw->isLoggedInContributor()) { ?>
-        <input type="submit" name="Submit" value="<?= $t->__('button.save') ?>">
-      <?php } ?>
-      <input type="button" value="<?= $t->__('button.cancel') ?>" onclick="window.location.href='bookSearch.php'" ></td>
-  </tr>
+</div>
 
 
-</table>
-</td>
-
-
-<td style="vertical-align:top">
-<?= $t->__('db.book.cover') ?><br/>
-<input id='cover_url' type="text" name="cover_url" value=<?= $fmw->getPostOrArrayQuoted($columns, 'cover_url') ?> size="52" onInput="loadImage();"/><br/><br/>
-<img id='imageContainer' src=<?= $fmw->getPostOrArrayQuoted($columns, 'cover_url') ?> width= "260" />
-</td>
-
+<div class="col-sm-5">
     
-</tr></table>
+  <div class="form-group">
+    <label class="control-label col-sm-2"><?= $t->__('db.book.cover') ?>:</label>
+    <div class="col-sm-10">
+        <input id='cover_url' type="text" class="form-control" name="cover_url" value=<?= $fmw->getPostOrArrayQuoted($columns, 'cover_url') ?> onInput="loadImage();"/><br/>
+<img id='imageContainer' src=<?= $fmw->getPostOrArrayQuoted($columns, 'cover_url') ?> width= "260" />
+    </div>
+  </div>
 
+</div>
+    
+</div>
+
+<!-- Buttons -->
+<div class="row">
+
+        <div class="col-sm-10">
+          <?php if ($fmw->isLoggedInContributor()) { ?>
+            <input type="submit" class="btn btn-default" name="Submit" value="<?= $t->__('button.save') ?>">
+          <?php } ?>
+          <input type="button" class="btn btn-default" value="<?= $t->__('button.cancel') ?>" onclick="window.location.href='bookSearch.php'" >
+        </div>
+
+</div>
+    
 </form>
 
 <?php include '_footer.php' ?>
